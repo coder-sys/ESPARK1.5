187b16bb15684f949378d913ef20c1edf010221c
coder-sys <71105356+coder-sys@users.noreply.github.com>
2022-07-03 14:54:33 -0700

    Update sign_in.js

--- sign_in.js
+++ sign_in.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
 import React,{useState} from 'react'
 import Particle_Background from './starbg';
-export default function SignIn (props){
+ export default function SignIn (props){
    
   const [fname,setFname] = useState("")
   const [lname,setLname] = useState("")
@@ -12,7 +12,9 @@ export default function SignIn (props){
   const [marginLeft,setMarginLeft] = useState(500)
   const [marginRight,setMarginRight] = useState(600)
   const [opacity,setOpacity] = useState(0.8)
- 
+  const [checked_lastname,setChecked_lastname] = useState('')
+  const [checked_firstname,setChecked_firstname] = useState('')
+  const [checked_email,setChecked_email] = useState('')
 
   return (
     <View >
@@ -27,13 +29,27 @@ export default function SignIn (props){
         <br></br>
         <TextInput placeholder='password' type='password' style={{padding:15,marginLeft:100,borderWidth:2,backgroundColor:'white'}} onChangeText={(e)=>(setPassword(e))}/><br></br>
         <button type='sumbit' style={{backgroundColor:"#3275A6",marginLeft:130,height:50,width:100,border:'none',borderRadius:10}} onClick={async()=>{
-          let api = await fetch(`https://e-spark.herokuapp.com/sign_in/${fname}/${lname}/${password}/${email}`)
+          let __api__ = await fetch(`http://localhost:8000/verify_sign_in_information/${fname}/${lname}`)
+          __api__ = await __api__.json()
+          alert(__api__['data'])
+          if(__api__['data'] == 'good to go!'){
+          let api = await fetch(`http://localhost:8000/sign_in/${fname}/${lname}/${password}/${email}`)
           let api_json = await api.json()
           props.navigation.navigate('Login')
-        }}><Text>Sign in</Text></button>
+          }
+        }}><Text>Sign in</Text></button><br></br>
+    
         </div>
 
     </View>
   );
   
 }
+//    <GoogleLogin 
+//clientId='595000170345-7mv8tj65rqe8e0lp5n2ilnsq7jfb1fn3.apps.googleusercontent.com'
+//buttonText='LOGIN'
+//onSuccess={(res)=>console.log(res)}
+//onFailure={(res)=>{console.log(res)}}
+//cookiePolicy={'single_host_origin'}
+//isSignedIn={true}cd 
+///>
